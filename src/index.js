2b7c829d5f1b5e8dab9c4c01f216a906e99e7e45
Pagrune <pagrune@orange.fr>
2023-10-16 08:15:23 +0200

    systeme client debut

--- src/index.js
+++ src/index.js
@@ -1,38 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Admin from './pages/Admin';
-import './style/style.scss';
-import reportWebVitals from './reportWebVitals';
-
 import io from 'socket.io-client';
 import Header from './components/Header';
 import Sav from './pages/Sav';
 import Client from './pages/Client';
 import Chat from './pages/Chat';
+import Admin from './pages/Admin';
+import './style/style.scss';
+import reportWebVitals from './reportWebVitals';
 
-const socket = io.connect('http://localhost:4000');
+const App = () => {
+  const [room, setRoom] = useState('');
+  const [title, setTitle] = useState('');
+  const [sujet, setSujet] = useState('');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Header/>
-    <Router>
-      <div className='App'>
-        <Routes>
-         <Route path='/admin' element={<Admin />} />
-         <Route path="/admin/:rubriqueSujet/:rubriqueId" element={<Sav/>} />
-         {/* <Route path="/news/:slug" element={<NewsDetail/>} /> */}
-         <Route path="/" element={<Client/>} />
-         <Route path="/chat" element={<Chat/>} />
-      
-        </Routes>
-      </div>
-    </Router>
-  </React.StrictMode>
-);
+  const socket = io.connect('http://localhost:4000');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  return (
+    <React.StrictMode>
+      <Header />
+      <Router>
+        <div className='App'>
+          <Routes>
+            <Route path='/admin' element={<Admin />} />
+            <Route path="/admin/:rubriqueSujet/:rubriqueId" element={<Sav />} />
+            {/* <Route path="/news/:slug" element={<NewsDetail/>} /> */}
+            <Route path="/" element={<Client 
+            room={room} 
+            setRoom={setRoom} 
+            sujet={sujet}
+            setSujet={setSujet}
+            title={title}
+            setTitle={setTitle}
+            socket={socket} />} />
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+        </div>
+      </Router>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
 reportWebVitals();
--- src/pages/Client.js
+++ src/pages/Client.js
@@ -1,41 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const Client = ({ username, setUsername, room, setRoom, socket }) => {
+const Client = ({ sujet, setSujet, room, setRoom, title, setTitle, socket }) => {
     const navigate = useNavigate();
+    const [sujets, setSujets] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/sujet')
+            .then(response => {
+                setSujets(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []); // Utilisez un tableau vide pour s'assurer que useEffect s'exécute une seule fois (lors du montage du composant)
 
     const joinRoom = () => {
-        if (room !== '' && username !== '') {
-          socket.emit('join_room', { username, room });
+        if (room !== '' && sujet !== '' && title !== '') {
+            socket.emit('join_room', { sujet, room, title });
         }
 
-        navigate('/chat', { replace: true });
-      };
-    
+        navigate('/chat', { replace: true }); // Assurez-vous que '/chat' est la bonne route vers votre page de chat
+    };
 
     return (
-      <div>
         <div>
-          {/* <input placeholder='Username...' onChange={(e) => setUsername(e.target.value)} />
-  
-          <select onChange={(e) => setRoom(e.target.value)}>
-            <option>-- Select Room --</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='node'>Node</option>
-            <option value='express'>Express</option>
-            <option value='react'>React</option>
-          </select> */}
-  
-          <button
-          className='btn btn-secondary'
-          style={{ width: '100%' }}
-          onClick={joinRoom} // Add this
-        >
-          Join the chat
-        </button>
+            <div>
+                {/* ... Votre code existant ... */}
+                <select onChange={(e) => setSujet(e.target.value)}>
+                    <option>-- Sélectionnez une salle --</option>
+                    {sujets.map(sujet => (
+                        <option key={sujet.id_sujet}>{sujet.sujet_rubrique}</option>
+                    ))}
+                </select>
+
+                 {/*Passer le sujet en paramètre de la route*/}
+            <div>Quel est le sujet de votre demande ?</div>
+            <input placeholder='Votre sujet de demande' onChange={(e) => setTitle(e.target.value)}/>
+
+                <button
+                    className='btn btn-secondary'
+                    style={{ width: '100%' }}
+                    onClick={joinRoom}
+                >
+                    Rejoindre le chat
+                </button>
+            </div>
         </div>
-      </div>
     );
-  };
-  
+};
+
 export default Client;
